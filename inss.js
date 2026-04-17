e9906e21447a896eac05ca7fe103be311630a50f
TRNS-DEV-DINHEIRO <thallysonraphael@gmail.com>
2026-04-17 20:30:50 -0300

    Add files via upload

--- inss.js
+++ inss.js
@@ -6,4 +6,25 @@ function cadastrar() {
         document.getElementById("vdesconto").value = vdesconto.toFixed(2);
         document.getElementById("liquido").value = Sliquido.toFixed(2);
     }
+else if(Sbruto<=2902.84){
+ let vdesconto = Sbruto * 0.07;
+        let Sliquido = Sbruto - vdesconto
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("liquido").value = Sliquido.toFixed(2);
+}
+else if(Sbruto<=4354.27){
+     let vdesconto = Sbruto * 0.12;
+        let Sliquido = Sbruto - vdesconto
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("liquido").value = Sliquido.toFixed(2);
+}
+else if(Sbruto<=8472.55) {
+     let vdesconto = Sbruto * 0.14;
+        let Sliquido = Sbruto - vdesconto
+        document.getElementById("vdesconto").value = vdesconto.toFixed(2);
+        document.getElementById("liquido").value = Sliquido.toFixed(2);
+}
+
+
+
 }
