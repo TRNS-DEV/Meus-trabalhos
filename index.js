3e5e8d6ab523cee2de1db76d09811d112ba66249
TRNS-DEV-DINHEIRO <thallysonraphael@gmail.com>
2026-04-29 09:08:26 -0300

    jogo quase pronto
    jogo da adivihação

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function jogo() {
-    let usuario = Number(document.getElementById("game").value);
-    let computer = Math.floor(Math.random() * 10) + 1;
-    let campo= document.getElementById("resultado");
-    if (usuario === computer) {
+   // 1. Pegamos o valor como TEXTO primeiro para validar
+    let campoInput = document.getElementById("game");
+    let valorBruto = campoInput.value;
+    //2.verificação de número
+    if(valorBruto===""){
+       alert("Por favor, digite um número antes de começar!");
+     return;
+    }
+    //3.ao passar pela verificação ira executar totalmente o código
+     let usuario=Number(valorBruto);
+     let computer=Math.floor(Math.random() * 10) + 1;
+     let campo=document.getElementById("resultado");
+     //4. condições para gnahar ou perder
+     if (usuario === computer) {
         document.getElementById("resultado").innerHTML = "você ganhou!";
     } else {
         document.getElementById("resultado").innerHTML = " você perdeu! o computador escolheu " + computer + " tente de novo ";
     }
-} document.getElementById("game").value="";
+ document.getElementById("game").value="";
+} 
