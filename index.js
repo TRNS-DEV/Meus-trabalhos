d91ad667f608cd00393d323215e65edc07d2ca85
TRNS-DEV-DINHEIRO <thallysonraphael@gmail.com>
2026-04-29 08:25:37 -0300

    jogo
    jogo da adivinhação com o computador

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-
-    function calcular() {
-    let compras = Number(document.getElementById("compras").value);
-    let Quantidade = Number(document.getElementById("quantidade").value);
-
-    let Nome = "";
-    let Preco = 0;
-    let total = 0;
-    let mensagem = "";
-
-    switch (compras) {
-        case 1:
-            Nome = "Hamburguer";
-            Preco = 10;
-            break;
-        case 2:
-            Nome = "Pizza";
-            Preco = 5;
-            break;
-        case 3:
-            Nome = "Coxinha";
-            Preco = 4;
-            break;
-        default:
-            mensagem = "Produto inválido!";
+function jogo() {
+    let usuario = Number(document.getElementById("game").value);
+    let computer = Math.floor(Math.random() * 10) + 1;
+    let campo= document.getElementById("resultado");
+    if (usuario === computer) {
+        document.getElementById("resultado").innerHTML = "você ganhou!";
+    } else {
+        document.getElementById("resultado").innerHTML = " você perdeu! o computador escolheu " + computer + " tente de novo ";
     }
-
-    // ✔️ Fora do switch (IMPORTANTE)
-    if (mensagem === "") {
-        total = Preco * Quantidade;
-
-        mensagem = `Produto: ${Nome} <br>
-        Quantidade: ${Quantidade} <br>
-        Total: R$ ${total}`;
-    }
-
-    document.getElementById("resultado").innerHTML = mensagem;
-}
-
+} document.getElementById("game").value="";
