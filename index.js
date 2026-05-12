2db4edcfc443993d3e2a3cc04167b2f9787c386e
TRNS-DEV <thallysonraphael@gmail.com>
2026-05-12 20:41:20 -0300

    lista de compras com array e for
    alegria, diversão e humor

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function login(){
-    let cpf= document.getElementById("CPF").value;
-    do{
-        if(cpf.length===14){
-            document.getElementById("msg").innerHTML="CPF valido!";
-            document.getElementById("CPF").value="";
-        }
-        else{
-            document.getElementById("msg").innerHTML="CPF invalido!";
-            document.getElementById("CPF").value="";
-            break;
-        }
-    } while(cpf.length!==14);
+function vendas() {
+    let quantidade = Number(document.getElementById("quantidade").value);
+    let texto = "";
+    for (let i = 1; i <= quantidade; i++) {
+        texto += "venda nº" + i + "<br>";
+        document.getElementById("resultado").innerHTML = texto;
+    }
 }
+let lista = [];
+function adicionar() {
+    let item = document.getElementById("item").value;
+    lista.push(item);
+    document.getElementById("resultado2").innerHTML = lista;
+}
+function mostrarlista() {
+    let texto = "";
+    for (let i = 0; i < lista.length; i++) {
+        texto += lista[i] + "<br>";
+        document.getElementById("resultado3").innerHTML = texto;
+    
+    }
+}
+    function adicionar2() {
+        let item2 = document.getElementById("item2").value;
+        lista.push(item2);
+        mostrarlista();
+    }
+    function excluir(){
+        lista.pop();
+        mostrarlista();
+    }
+
+
