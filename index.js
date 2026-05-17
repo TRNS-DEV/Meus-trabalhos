4ed413968851db055834ee1d518d9fef57685298
TRNS-DEV <thallysonraphael@gmail.com>
2026-05-17 19:43:07 -0300

    Cadastro
    Cadastros feitos utilizando while, Do while e for

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-function vendas() {
-    let quantidade = Number(document.getElementById("quantidade").value);
-    let texto = "";
-    for (let i = 1; i <= quantidade; i++) {
-        texto += "venda nº" + i + "<br>";
-        document.getElementById("resultado").innerHTML = texto;
+function cadastro(){
+    let vagas=7;
+    let texto="";
+    while(vagas>0){
+        texto+="produto cadastrado com sucesso!"+"<br>";
+        vagas--
+
     }
+    texto+="ops não há mais vagas disponiveis";
+    document.getElementById("result").innerHTML=texto;
 }
-let lista = [];
-function adicionar() {
-    let item = document.getElementById("item").value;
-    lista.push(item);
-    document.getElementById("resultado2").innerHTML = lista;
+function login(){
+    let continuar;
+    let texto="";
+
+    do{
+        texto+="produto cadastrado "+"<br>"
+        continuar=prompt("cadastrar outro produto? S/N");
+    }while(continuar=="S")
+        document.getElementById("resposta").innerHTML=texto;
 }
-function mostrarlista() {
-    let texto = "";
-    for (let i = 0; i < lista.length; i++) {
-        texto += lista[i] + "<br>";
-        document.getElementById("resultado3").innerHTML = texto;
+function cadastroFor(){
+    let texto="";
+    for(let i=1;i<=15;i++){
+        texto+="produto"+ i +"cadastrado"+"<br>";
     
     }
+    document.getElementById("resultado").innerHTML=texto;
 }
-    function adicionar2() {
-        let item2 = document.getElementById("item2").value;
-        lista.push(item2);
-        mostrarlista();
-    }
-    function excluir(){
-        lista.pop();
-        mostrarlista();
-    }
 
-
+    
