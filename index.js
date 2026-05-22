8c6aeb20fe40e12f4f84370b1f6c644d815d99bf
TRNS-DEV <thallysonraphael@gmail.com>
2026-05-22 16:33:58 -0300

    update

--- index.js
+++ index.js
@@ -3,36 +3,41 @@ formulario.addEventListener("submit", function(evento){
     //1.interroper o recarregamento da pagina
     evento.preventDefault();
     // salavando os valores que vão estar nas caixas de notas do primeiro trimestre//
-    const first=Number(document.getElementById("t1-nota1").value);
-    const second=Number(document.getElementById("t1-nota2").value);
-    const third = Number(document.getElementById("t1-nota3").value);
-    const mediaTrimestre1= (first+ second+ third)/3;
+    const t1Nota1=Number(document.getElementById("t1-nota1").value);
+    const t1Nota2=Number(document.getElementById("t1-nota2").value);
+    const t1Nota3 = Number(document.getElementById("t1-nota3").value);
+    const mediaTrimestre1= (t1Nota1+ t1Nota2+ t1Nota3)/3;
 
 //segundo trimestre e seu calculo
-const fourth=Number(document.getElementById("t2-nota1").value);
- const fifth=Number(document.getElementById("t2-nota2").value);
- const sixth= Number(document.getElementById("t2-nota3").value);
- const mediaTrimestre2= (fourth+ fifth+ sixth)/3;
+const t2Nota1=Number(document.getElementById("t2-nota1").value);
+ const t2Nota2=Number(document.getElementById("t2-nota2").value);
+ const t2Nota3= Number(document.getElementById("t2-nota3").value);
+ const mediaTrimestre2= (t2Nota1+ t2Nota2 + t2Nota3)/3;
  
 //Terceiro trimestre e seu calculo
-const seventh=Number(document.getElementById("t3-nota1").value);
- const eighth=Number(document.getElementById("t3-nota2").value);
- const ninth= Number(document.getElementById("t3-nota3").value);
- const mediaTrimestre3= (seventh+ eighth+ ninth)/3;
+const t3Nota1=Number(document.getElementById("t3-nota1").value);
+ const t3Nota2=Number(document.getElementById("t3-nota2").value);
+ const t3Nota3= Number(document.getElementById("t3-nota3").value);
+ const mediaTrimestre3= (t3Nota1+ t3Nota2+ t3Nota3)/3;
 
 //Calculo da media Anual
 const mediaAnual=(mediaTrimestre1 + mediaTrimestre2 + mediaTrimestre3)/3
 if (mediaAnual >= 6) {
         document.getElementById("painel-resultado").innerHTML = `
-            <h3>Parabéns! Você foi aprovado!</h3>
+        <h3>Parabéns! Você foi aprovado!</h3>
             <p>Média T1: ${mediaTrimestre1.toFixed(1)} | Média T2: ${mediaTrimestre2.toFixed(1)} | Média T3: ${mediaTrimestre3.toFixed(1)}</p>
-            <p><strong>Média Final Anual: ${mediaAnual.toFixed(1)}</strong></p>
-        `;
+            <p><strong>Média Final Anual: ${mediaAnual.toFixed(1)}</strong></p>`
+        document.getElementById("painel-resultado").classList.remove("recuperacao");
+document.getElementById("painel-resultado").classList.add("aprovado");
+        
     } else {
         document.getElementById("painel-resultado").innerHTML = `
-            <h3>Média anual abaixo do necessário. Você está de recuperação.</h3>
+            
+        <h3>Média anual abaixo do necessário. Você está de recuperação.</h3>
             <p>Média T1: ${mediaTrimestre1.toFixed(1)} | Média T2: ${mediaTrimestre2.toFixed(1)} | Média T3: ${mediaTrimestre3.toFixed(1)}</p>
-            <p><strong>Média Final Anual: ${mediaAnual.toFixed(1)}</strong></p>
-        `;
+            <p><strong>Média Final Anual: ${mediaAnual.toFixed(1)}</strong></p>`
+            document.getElementById("painel-resultado").classList.remove("aprovado");
+document.getElementById("painel-resultado").classList.add("recuperacao");
+        ;
     }
 });
